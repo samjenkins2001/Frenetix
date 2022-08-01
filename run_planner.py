d70ed25105cf4a4e81a314f096c52bb2751ab39f
rainer <ga97neq@mytum.de>
2022-08-01 17:51:00 +0200

    change path settings

--- run_planner.py
+++ run_planner.py
@@ -1,12 +1,21 @@
+import os
+import sys
 from commonroad_rp.run_planner import run_planner
 from commonroad_rp.configuration_builder import ConfigurationBuilder
 
+mopl_path = os.path.dirname(
+    os.path.abspath(__file__)
+)
+sys.path.append(mopl_path)
+
+
 if __name__ == '__main__':
 
-    scenario_name = "/media/sf_6.Semester/Planner/commonroad-reactive-planner/example_scenarios/DEU_Flensburg-24_1_T-1"
+    scenario_name = "DEU_Flensburg-24_1_T-1"
+    scenario_path = os.path.join(mopl_path, "example_scenarios", scenario_name)
 
-    config = ConfigurationBuilder.build_configuration(scenario_name+".xml")
-    log_path = "./logs/"+scenario_name.split("/")[-1]
+    config = ConfigurationBuilder.build_configuration(scenario_path+".xml")
+    log_path = "./logs/"+scenario_path.split("/")[-1]
     cost_function_path = "configurations/cost_weights.yaml"
 
     run_planner(config, log_path, cost_function_path)
