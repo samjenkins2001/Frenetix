577a27c56801b44c13673a9382bde5f8e294bf02
rainer <ga97neq@mytum.de>
2022-10-06 18:05:47 +0200

    new config

--- run_planner.py
+++ run_planner.py
@@ -11,9 +11,9 @@ sys.path.append(mopl_path)
 
 if __name__ == '__main__':
 
-    scenario_name = "ZAM_Tjunction-1_81_T-1"
+    scenario_name = "DEU_Ibbenbueren-16_1_T-1"
     # scenario_path = os.path.join(mopl_path, "commonroad-reactive-planner", "example_scenarios", scenario_name)
-    scenario_path = os.path.join(mopl_path, "commonroad-scenarios", "scenarios", "recorded", "hand-crafted", scenario_name)
+    scenario_path = os.path.join(mopl_path, "commonroad-scenarios", "scenarios", "recorded", "scenario-factory", scenario_name)
     config = ConfigurationBuilder.build_configuration(scenario_path+".xml")
     log_path = "./logs/"+scenario_path.split("/")[-1]
     cost_function_path = "configurations/cost_weights.yaml"
