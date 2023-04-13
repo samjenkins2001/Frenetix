6e0dc56983e14bda2b4e62d7c2557868430dfd75
rainer <ga97neq@mytum.de>
2023-04-13 17:32:33 +0200

    make structure beautiful

--- run_planner.py
+++ run_planner.py
@@ -13,12 +13,10 @@ stack_path = os.path.dirname(os.path.dirname(
 
 if __name__ == '__main__':
 
-    scenario_name = "ZAM_Tjunction-1_294_T-1"
+    scenario_name = "USA_US101-22_3_T-1"
     scenario_path = os.path.join(stack_path, "commonroad-scenarios", "scenarios", scenario_name)
 
     config = ConfigurationBuilder.build_configuration(scenario_path+".xml")
-
     log_path = "./logs/"+scenario_path.split("/")[-1]
-    cost_function_path = "configurations/cost_weights.yaml"
 
     run_planner(config, log_path, mod_path)
