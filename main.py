680beb91aa7a14fb0515acf25206a1e05a8060ad
ga97neq@mytum.de <ga97neq@mytum.de>
2023-08-08 12:50:49 +0200

    add basic pipeline

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from os import listdir
+from os.path import isfile, join
 from commonroad_rp.run_planner import run_planner
 from cr_scenario_handler.simulation.simulation import Simulation
 from cr_scenario_handler.utils.configuration_builder import ConfigurationBuilder
@@ -17,18 +19,27 @@ stack_path = os.path.dirname(os.path.dirname(
 
 if __name__ == '__main__':
 
-    scenario_name = "ZAM_Tjunction-1_100_T-1"
-    scenario_path = os.path.join(stack_path, "commonroad-scenarios", "scenarios", scenario_name)
-
-    config = ConfigurationBuilder.build_configuration(scenario_path+".xml")
-    log_path = "./logs/"+scenario_path.split("/")[-1]
-
     start_multiagent = False
     use_cpp = False
-    
-    if not start_multiagent:
-        run_planner(config, log_path, mod_path, use_cpp)
-    else:
-        # Works only with wale-net. Ground Truth Prediction not possible!
-        simulation = Simulation(config, log_path, mod_path)
-        simulation.run_simulation()
+    evaluation_pipeline = False
+
+    scenario_name = "ZAM_Tjunction-1_100_T-1.xml"
+    scenario_folder = os.path.join(stack_path, "commonroad-scenarios", "scenarios")
+
+    scenario_files = [f for f in listdir(scenario_folder) if isfile(join(scenario_folder, f))]
+    number_of_runs = len(scenario_files) if evaluation_pipeline else 1
+
+    for runs in range(0, number_of_runs):
+
+        scenario_path = os.path.join(scenario_folder, scenario_files[runs]) if evaluation_pipeline \
+            else os.path.join(scenario_folder, scenario_name)
+
+        config = ConfigurationBuilder.build_configuration(scenario_path.split(".")[-2] + ".xml")
+        log_path = "./logs/"+scenario_path.split("/")[-1]
+
+        if not start_multiagent:
+            run_planner(config, log_path, mod_path, use_cpp)
+        else:
+            # Works only with wale-net. Ground Truth Prediction not possible!
+            simulation = Simulation(config, log_path, mod_path)
+            simulation.run_simulation()
